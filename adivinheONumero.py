b28cff621344cbed984bee24d3c71dacaaf1b5ef
Bruno Quindeler <21955104+Brunoquindeler@users.noreply.github.com>
2020-09-03 07:59:28 -0300

    Update adivinheONumero.py

--- adivinheONumero.py
+++ adivinheONumero.py
@@ -6,20 +6,20 @@ def main():
     try:
         nEscolhido = int(input('Escolha um número de 1 a 10: '))
     except:
-        print('Isto não é um número.')
+        print('Isto não é um número.\n')
         main()
 
     if nEscolhido > 10:
-        print('Número inválido.')
+        print('Número inválido.\n')
         main()
 
     n = random.randint(1, 10)
-    print('Número gerado:', n)
+    print('\nNúmero gerado:', n)
 
     if n == nEscolhido:
-        print('ACERTOU!!!')
+        print('ACERTOU!!!\n')
     else:
-        print('ERROU...')
+        print('ERROU...\n')
         main()
 
 if __name__=='__main__':
